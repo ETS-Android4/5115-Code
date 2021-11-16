db6d469dbd7fadb94aec107b397c0793676110f5
EnderHawkeye <32406140+EnderHawkeye@users.noreply.github.com>
2021-11-16 16:21:14 -0700

    fixed errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTestDrive.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 
-@Autonomous(name="Autonomous Drive", group="HardwarePushbot")
+@Autonomous(name="Autonomous Test Drive", group="HardwarePushbot")
 
 public class AutonomousTestDrive extends LinearOpMode {
 
