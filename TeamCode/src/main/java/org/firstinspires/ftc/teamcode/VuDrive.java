5e7b7ef592333e4aa73337415b290d3cddebcef9
cdavidson22 <cdavidson22@gmail.com>
2022-02-04 12:02:33 -0700

    some changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuDrive.java
@@ -39,6 +39,11 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -77,9 +82,9 @@ import java.util.List;
  * is explained below.
  */
 
-@TeleOp(name="Vuforia Field Nav Webcam", group ="Concept")
+@TeleOp(name="Vuforia Driving", group ="Concept")
 //@Disabled
-public class VuPosTest extends LinearOpMode {
+public class VuDrive extends LinearOpMode {
 
     /*
      * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
@@ -116,6 +121,19 @@ public class VuPosTest extends LinearOpMode {
     private WebcamName webcamName       = null;
 
     private boolean targetVisible       = false;
+
+    //drive system stuff
+    private final ElapsedTime runtime = new ElapsedTime();
+    private DcMotor leftDrive = null;
+    private DcMotor rightDrive = null;
+    private DcMotor spinner = null;
+    private DcMotor arm = null;
+    private Servo leftClaw = null;
+    private Servo rightClaw = null;
+    private String status = "Off";
+    private String speed = "No value";
+    private double posLeft = 0;
+    private double posRight = 1;
 
     @Override public void runOpMode() {
         // Connect to the camera we are to use.  This name must match what is set up in Robot Configuration
@@ -212,13 +230,36 @@ public class VuPosTest extends LinearOpMode {
         final float CAMERA_LEFT_DISPLACEMENT     = 0.0f * mmPerInch;   // eg: Enter the left distance from the center of the robot to the camera lens
 
         OpenGLMatrix cameraLocationOnRobot = OpenGLMatrix
-                    .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
-                    .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
+                .translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+                .multiplied(Orientation.getRotationMatrix(EXTRINSIC, XZY, DEGREES, 90, 90, 0));
 
         /**  Let all the trackable listeners know where the camera is.  */
         for (VuforiaTrackable trackable : allTrackables) {
             ((VuforiaTrackableDefaultListener) trackable.getListener()).setCameraLocationOnRobot(parameters.cameraName, cameraLocationOnRobot);
         }
+
+        leftDrive = hardwareMap.get(DcMotor.class, "left");
+        rightDrive = hardwareMap.get(DcMotor.class, "right");
+        spinner = hardwareMap.get(DcMotor.class, "spinner");
+        arm = hardwareMap.get(DcMotor.class, "arm");
+        leftClaw = hardwareMap.get(Servo.class, "leftClaw");
+        rightClaw = hardwareMap.get(Servo.class, "rightClaw");
+
+        rightClaw.setPosition(posLeft);
+        leftClaw.setPosition(posRight);
+
+        telemetry.addData("Status", status);
+        telemetry.addData("Speed", speed);
+        telemetry.addData("Left servo pos", posLeft);
+        telemetry.addData("Right servo pos", posRight);
+        telemetry.update();
+
+        // Most robots need the motor on one side to be reversed to drive forward
+        // Reverse the motor that runs backwards when connected directly to the battery
+        leftDrive.setDirection(DcMotor.Direction.FORWARD);
+        rightDrive.setDirection(DcMotor.Direction.REVERSE);
+        spinner.setDirection(DcMotor.Direction.FORWARD);
+        arm.setDirection(DcMotor.Direction.FORWARD);
 
         /*
          * WARNING:
@@ -228,7 +269,11 @@ public class VuPosTest extends LinearOpMode {
          * To restore the normal opmode structure, just un-comment the following line:
          */
 
-        // waitForStart();
+
+
+         waitForStart();
+
+
 
         /* Note: To use the remote camera preview:
          * AFTER you hit Init on the Driver Station, use the "options menu" to select "Camera Stream"
@@ -236,6 +281,128 @@ public class VuPosTest extends LinearOpMode {
          * It is not permitted to transition to RUN while the camera preview window is active.
          * Either press STOP to exit the OpMode, or use the "options menu" again, and select "Camera Stream" to close the preview window.
          */
+
+        boolean slow = false;
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            // Setup a variable for each drive wheel to save power level for telemetry
+            double leftPower;
+            double rightPower;
+            // Set the speed as a variable to reduce space usage
+            double leftSpeed = 1;
+            double rightSpeed = 1;
+            double spinnerSpeed = 0;
+            double armSpeed = 0;
+            // Sets the string values for telemetry
+            status = "On";
+            speed = "Fast";
+
+            //variable for claw position
+            double lcPos = leftClaw.getPosition(); //Setting left Claw position
+            double rcPos = rightClaw.getPosition(); //Setting Right Claw position
+
+
+            //space where slowing code will go
+            if (gamepad1.y) {
+                if (slow == false) {
+                    slow = true;
+                } else {
+                    slow = false;
+                }
+            }
+            if (slow && gamepad1.a) {
+                spinner.setDirection(DcMotor.Direction.FORWARD);
+                leftSpeed = 0.5;
+                rightSpeed = 0.5;
+                spinnerSpeed = 0.5;
+                status = "Spinner forward";
+                speed = "Slow";
+//                telemetry.addData("Spinner", "On and Front Spinning");
+//                telemetry.addData("Speed", "Slow");
+            }
+            if (slow && gamepad1.b) {
+                spinner.setDirection(DcMotor.Direction.REVERSE);
+                leftSpeed = 0.5;
+                rightSpeed = 0.5;
+                spinnerSpeed = 0.5;
+                status = "Spinner Backwards";
+                speed = "Slow";
+//                telemetry.addData("Spinner", "On and Front Spinning");
+//                telemetry.addData("Speed", "Slow");
+            } else if (slow) {
+                leftSpeed = 0.5;
+                rightSpeed = 0.5;
+//                telemetry.addData("Speed", "Slow");
+                speed = "slow";
+            } else if (gamepad1.a) {
+                spinner.setDirection(DcMotor.Direction.FORWARD);
+                spinnerSpeed = 0.5;
+//                telemetry.addData("Spinner", "On and Front Spinning");
+                status = "Spinner forward";
+                speed = "Fast";
+            } else if (gamepad1.b) {
+                spinner.setDirection(DcMotor.Direction.REVERSE);
+                spinnerSpeed = 0.5;
+//                telemetry.addData("Spinner", "On and Back Spinning");
+                status = "spinner backwards";
+                speed = "Fast";
+            } else {
+                leftSpeed = 1;
+                rightSpeed = 1;
+                spinnerSpeed = 0;
+                spinner.setDirection(DcMotor.Direction.FORWARD);
+                status = "drive";
+                speed = "Fast";
+//                telemetry.addData("Speed", "Fast");
+            }
+//                This controls the arm lol
+            if (gamepad1.dpad_up || gamepad1.left_trigger > 0) {
+                //move arm up
+                arm.setDirection(DcMotorSimple.Direction.REVERSE);
+                arm.setPower(1);
+                status = "arm up";
+            } else if (gamepad1.dpad_down || gamepad1.right_trigger > 0) {
+                //move claw down
+                arm.setDirection(DcMotorSimple.Direction.FORWARD);
+                arm.setPower(1);
+                status = "arm down";
+            } else {
+                arm.setPower(0);
+                status = null;
+            }
+
+            //These control the servos
+            if (gamepad1.dpad_right || gamepad1.right_bumper) {
+                leftClaw.setPosition(lcPos -= 0.1);
+                rightClaw.setPosition(rcPos += 0.1);
+            } else if (gamepad1.dpad_left || gamepad1.left_bumper) {
+                leftClaw.setPosition(lcPos += 0.1);
+                rightClaw.setPosition(rcPos -= 0.1);
+            }
+
+            double drive = -gamepad1.left_stick_y;
+            double turn = gamepad1.right_stick_x;
+            leftPower = Range.clip(drive + turn, -leftSpeed, leftSpeed);
+            rightPower = Range.clip(drive - turn, -rightSpeed, rightSpeed);
+
+
+            // Send calculated power to wheels
+            leftDrive.setPower(-leftPower);
+            rightDrive.setPower(-rightPower);
+            spinner.setPower(spinnerSpeed);
+            arm.setPower(armSpeed);
+
+
+            // Show the elapsed game time and wheel power.
+            speed = "Fast";
+            telemetry.addData("Run time", runtime.toString());
+            telemetry.addData("Status", status);
+            telemetry.addData("speed", speed);
+            telemetry.update();
+        }
 
         targets.activate();
         while (!isStopRequested()) {
@@ -273,6 +440,8 @@ public class VuPosTest extends LinearOpMode {
             }
             telemetry.update();
         }
+
+
 
         // Disable Tracking when we are done;
         targets.deactivate();
